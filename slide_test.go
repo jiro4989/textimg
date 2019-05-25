1cd94988ef4e7deb8ee21d864952e5f659a33a7f
jiro4989 <jiroron666@gmail.com>
2019-05-25 12:42:22 +0900

    :white_check_mark: テスト追加

--- slide_test.go
+++ slide_test.go
@@ -15,6 +15,31 @@ func TestToSlideStrings(t *testing.T) {
 	}
 	tds := []TestData{
 		{
+			desc: "2行描画、スライド幅1、無限なし",
+			src:  []string{"1", "2", "3", "4", "5"},
+			expect: []string{
+				"1", "2",
+				"2", "3",
+				"3", "4",
+				"4", "5",
+			},
+			lineCount:    2,
+			slideWidth:   1,
+			slideForever: false,
+		},
+		{
+			desc: "2行描画、スライド幅2、無限なし",
+			src:  []string{"1", "2", "3", "4", "5"},
+			expect: []string{
+				"1", "2",
+				"3", "4",
+				"5", "",
+			},
+			lineCount:    2,
+			slideWidth:   2,
+			slideForever: false,
+		},
+		{
 			desc: "3行描画、スライド幅1、無限なし",
 			src:  []string{"1", "2", "3", "4", "5"},
 			expect: []string{
@@ -74,6 +99,42 @@ func TestToSlideStrings(t *testing.T) {
 			slideForever: false,
 		},
 		{
+			desc: "3行描画、スライド幅3、無限なし、不足あり",
+			src:  []string{"1", "2", "3", "4", "5", "6", "7", "8"},
+			expect: []string{
+				"1", "2", "3",
+				"4", "5", "6",
+				"7", "8", "",
+			},
+			lineCount:    3,
+			slideWidth:   3,
+			slideForever: false,
+		},
+		{
+			desc: "2行描画、スライド幅2、無限あり",
+			src:  []string{"1", "2", "3", "4", "5"},
+			expect: []string{
+				"1", "2",
+				"3", "4",
+				"5", "1",
+			},
+			lineCount:    2,
+			slideWidth:   2,
+			slideForever: true,
+		},
+		{
+			desc: "2行描画、スライド幅2、無限あり",
+			src:  []string{"1", "2", "3", "4", "5", "6"},
+			expect: []string{
+				"1", "2",
+				"3", "4",
+				"5", "6",
+			},
+			lineCount:    2,
+			slideWidth:   2,
+			slideForever: true,
+		},
+		{
 			desc: "3行描画、スライド幅1、無限あり",
 			src:  []string{"1", "2", "3", "4", "5"},
 			expect: []string{
@@ -82,6 +143,21 @@ func TestToSlideStrings(t *testing.T) {
 				"3", "4", "5",
 				"4", "5", "1",
 				"5", "1", "2",
+			},
+			lineCount:    3,
+			slideWidth:   1,
+			slideForever: true,
+		},
+		{
+			desc: "3行描画、スライド幅1、無限あり",
+			src:  []string{"1", "2", "3", "4", "5", "6"},
+			expect: []string{
+				"1", "2", "3",
+				"2", "3", "4",
+				"3", "4", "5",
+				"4", "5", "6",
+				"5", "6", "1",
+				"6", "1", "2",
 			},
 			lineCount:    3,
 			slideWidth:   1,
@@ -97,6 +173,41 @@ func TestToSlideStrings(t *testing.T) {
 			},
 			lineCount:    3,
 			slideWidth:   2,
+			slideForever: true,
+		},
+		{
+			desc: "3行描画、スライド幅2、無限あり",
+			src:  []string{"1", "2", "3", "4", "5", "6"},
+			expect: []string{
+				"1", "2", "3",
+				"3", "4", "5",
+				"5", "6", "1",
+			},
+			lineCount:    3,
+			slideWidth:   2,
+			slideForever: true,
+		},
+		{
+			desc: "3行描画、スライド幅3、無限あり",
+			src:  []string{"1", "2", "3", "4", "5", "6"},
+			expect: []string{
+				"1", "2", "3",
+				"4", "5", "6",
+			},
+			lineCount:    3,
+			slideWidth:   3,
+			slideForever: true,
+		},
+		{
+			desc: "3行描画、スライド幅3、無限あり",
+			src:  []string{"1", "2", "3", "4", "5", "6", "7"},
+			expect: []string{
+				"1", "2", "3",
+				"4", "5", "6",
+				"7", "1", "2",
+			},
+			lineCount:    3,
+			slideWidth:   3,
 			slideForever: true,
 		},
 	}
