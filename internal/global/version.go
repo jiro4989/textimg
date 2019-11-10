d1e3380ea166e38c17cb15f9726b7a1ecc9b535d
jiro4989 <jiroron666@gmail.com>
2019-11-10 21:01:06 +0900

    :bulb: バージョンアップ

--- internal/global/version.go
+++ internal/global/version.go
@@ -2,7 +2,7 @@ package global
 
 const (
 	AppName = "textimg"
-	Version = `2.1.0
+	Version = `2.2.0
 Copyright (c) 2019 jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/textimg`
