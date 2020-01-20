a0eab05f837ef39f83970f226635ddcbefae4204
jiro4989 <jiroron666@gmail.com>
2020-01-21 07:26:01 +0900

    :arrow_up: バージョンアップ #95

--- internal/global/version.go
+++ internal/global/version.go
@@ -2,7 +2,7 @@ package global
 
 const (
 	AppName = "textimg"
-	Version = `2.3.1
+	Version = `2.3.2
 Copyright (c) 2019 jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/textimg`
