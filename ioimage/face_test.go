a7b9439159a9dc52d3828e000264a9e734912c03
jiro4989 <jiroron666@gmail.com>
2021-03-21 11:49:43 +0900

    'go fmt' した

--- ioimage/face_test.go
+++ ioimage/face_test.go
@@ -11,47 +11,47 @@ func TestReadFace(t *testing.T) {
 	testdataDir := filepath.Join("..", "testdata", "in")
 
 	type TestData struct {
-		desc          string
-		inFontPath    string
-		inFontIndex   int
-		wantErr bool
+		desc        string
+		inFontPath  string
+		inFontIndex int
+		wantErr     bool
 	}
 	tests := []TestData{
 		{
-			desc:          "正常系: font.Faceが取得できる",
-			inFontPath:    "/tmp/MyricaM.TTC",
+			desc:        "正常系: font.Faceが取得できる",
+			inFontPath:  "/tmp/MyricaM.TTC",
 			inFontIndex: 0,
-			wantErr: false,
+			wantErr:     false,
 		},
 		{
-			desc:          "正常系: 存在しないファイルの場合もエラーにはならない",
-			inFontPath:    "/tmp/寿司",
+			desc:        "正常系: 存在しないファイルの場合もエラーにはならない",
+			inFontPath:  "/tmp/寿司",
 			inFontIndex: 0,
-			wantErr: false,
+			wantErr:     false,
 		},
 		{
-			desc:          "異常系: パスとしては存在するがディレクトリの場合はエラー",
-			inFontPath:    "/tmp",
+			desc:        "異常系: パスとしては存在するがディレクトリの場合はエラー",
+			inFontPath:  "/tmp",
 			inFontIndex: 0,
-			wantErr: true,
+			wantErr:     true,
 		},
 		{
-			desc:          "異常系: ファイルは存在するけれど、フォントファイルじゃない時はエラー (ttc)",
-			inFontPath:    filepath.Join(testdataDir, "illegal_font.ttc"),
+			desc:        "異常系: ファイルは存在するけれど、フォントファイルじゃない時はエラー (ttc)",
+			inFontPath:  filepath.Join(testdataDir, "illegal_font.ttc"),
 			inFontIndex: 0,
-			wantErr: true,
+			wantErr:     true,
 		},
 		{
-			desc:          "異常系: ファイルは存在するけれど、フォントファイルじゃない時はエラー (otc)",
-			inFontPath:    filepath.Join(testdataDir, "illegal_font.otc"),
+			desc:        "異常系: ファイルは存在するけれど、フォントファイルじゃない時はエラー (otc)",
+			inFontPath:  filepath.Join(testdataDir, "illegal_font.otc"),
 			inFontIndex: 0,
-			wantErr: true,
+			wantErr:     true,
 		},
 		{
-			desc:          "異常系: ファイルは存在するけれど、フォントファイルじゃない時はエラー (txt)",
-			inFontPath:    filepath.Join(testdataDir, "illegal_font.txt"),
+			desc:        "異常系: ファイルは存在するけれど、フォントファイルじゃない時はエラー (txt)",
+			inFontPath:  filepath.Join(testdataDir, "illegal_font.txt"),
 			inFontIndex: 0,
-			wantErr: true,
+			wantErr:     true,
 		},
 	}
 	for _, tt := range tests {
