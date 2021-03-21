5d404fae4ccbf0c2ac1ffaace864404343c163bd
jiro4989 <jiroron666@gmail.com>
2021-03-21 11:10:21 +0900

    Main実行でフォント指定の異常系テストを追加

--- main_test.go
+++ main_test.go
@@ -58,6 +58,16 @@ func TestMainNormal(t *testing.T) {
 			in:   []string{"", "Sample", "-o", outDir + "/main_normal_red.png", "-g", "green", "-b", "bbbb"},
 			want: -1,
 		},
+		{
+			desc: "異常系: 存在しないFontを指定",
+			in:   []string{"", "Sample", "-o", outDir + "/illegal_case1.png", "-f", "./寿司.txt"},
+			want: -1,
+		},
+		{
+			desc: "異常系: 存在しない絵文字Fontを指定",
+			in:   []string{"", "Sample", "-o", outDir + "/illegal_case2.png", "-e", "./寿司.txt"},
+			want: -1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
