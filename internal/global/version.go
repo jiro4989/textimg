2d9a42db7d7d61df88f784a598818e5da8a183ff
jiro4989 <jiroron666@gmail.com>
2026-09-14 22:20:37 +0900

    chore: バージョン情報を VCS から取得するように変更 #378

--- internal/global/version.go
+++ internal/global/version.go
@@ -1,9 +1,23 @@
 package global
 
+import "runtime/debug"
+
 const (
 	AppName = "textimg"
-	Version = `3.2.2
+)
+
+var (
+	Version = "dev"
+)
+
+func init() {
+	const copyright = `
 Copyright (c) 2019 jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/textimg`
-)
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		v := info.Main.Version
+		Version = v + copyright
+	}
+}
