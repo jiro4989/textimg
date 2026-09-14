522bf3ff7809c24691f7cf9ebbf5363cda243f80
jiro <jiroron666@gmail.com>
2026-09-14 23:06:10 +0900

    ♻️ internal/global パッケージを config/version パッケージに変更 (#380)
    * del: 未使用のコードを削除
    
    * fix: internal/global パッケージを config/version パッケージに変更
    
    * chore: interface を any に変更

--- log/log.go
+++ log/log.go
@@ -6,7 +6,7 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/jiro4989/textimg/v3/internal/global"
+	"github.com/jiro4989/textimg/v3/config/version"
 )
 
 const (
@@ -16,7 +16,7 @@ const (
 	errorPrefix = "[ERROR]"
 )
 
-func log(lvl string, msg interface{}) {
+func log(lvl string, msg any) {
 	_, f, l, ok := runtime.Caller(2)
 	if !ok {
 		fmt.Fprintln(os.Stderr, "something error occurred.")
@@ -24,27 +24,27 @@ func log(lvl string, msg interface{}) {
 	}
 
 	now := time.Now().Format("2006/01/02 03:04:05")
-	text := fmt.Sprintf("%s %s %s %s:%d %v", now, global.AppName, lvl, f, l, msg)
+	text := fmt.Sprintf("%s %s %s %s:%d %v", now, version.AppName, lvl, f, l, msg)
 	fmt.Fprintln(os.Stderr, text)
 }
 
-func Debug(msg interface{}) {
+func Debug(msg any) {
 	log(debugPrefix, msg)
 }
 
-func Info(msg interface{}) {
+func Info(msg any) {
 	log(infoPrefix, msg)
 }
 
-func Warn(msg interface{}) {
+func Warn(msg any) {
 	log(warnPrefix, msg)
 }
 
-func Warnf(format string, msg interface{}) {
+func Warnf(format string, msg any) {
 	text := fmt.Sprintf(format, msg)
 	log(warnPrefix, text)
 }
 
-func Error(msg interface{}) {
+func Error(msg any) {
 	log(errorPrefix, msg)
 }
